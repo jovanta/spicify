df511438f7e319dca4c12fa8ddb17da888052c52
ddhyuliani <ddhyuliani@gmail.com>
2021-06-08 00:09:00 +0700

    Add Information Storyboard & ViewController
    Add several assets, information.storyboard, and informationViewController

---
+++ Spicify/Spicify/ViewController/InformationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InformationViewController.swift
+//  Spicify
+//
+//  Created by Dian Dinihari on 07/06/21.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
