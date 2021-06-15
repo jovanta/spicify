0c461e4167ec9e43f5a88c3801a85fffb611f501
Elkana Antonio <elkanarahadian@gmail.com>
2021-06-15 13:25:39 +0700

    Update Library

--- Spicify/Spicify/ViewController/LibraryTableVC.swift
+++ Spicify/Spicify/ViewController/LibraryTableVC.swift
@@ -2,12 +2,8 @@
 import UIKit
 
 class LibraryTableVC: UITableViewCell {
-
     @IBOutlet weak var libraryImageView: UIImageView!
-
-    
     @IBOutlet weak var spicesImageView: UIImageView!
-    
     @IBOutlet weak var spicesTitleLabel: UILabel!
     @IBOutlet weak var spicesView: UIView!
     @IBOutlet weak var spicesSubLabel: UILabel!
--- Spicify/Spicify/ViewController/LibraryVC.swift
+++ Spicify/Spicify/ViewController/LibraryVC.swift
@@ -3,8 +3,15 @@ import UIKit
 
 class LibraryVC: UIViewController, UITableViewDataSource {
     
+    let spicesLibrary = [
+        spiceLibrary(titleName: "Ginger", subName: "Zingiber officinale", spiceImage: #imageLiteral(resourceName: "Ginger Main")),
+        spiceLibrary(titleName: "Turmeric", subName: "Curcuma longa", spiceImage: #imageLiteral(resourceName: "Turmeric Main")),
+        spiceLibrary(titleName: "Galangal", subName: "Alpinia galanga", spiceImage: #imageLiteral(resourceName: "Galangal Main")),
+        spiceLibrary(titleName: "Aromatic Ginger", subName: "Kaempferia galanga", spiceImage: #imageLiteral(resourceName: "Aromatic Ginger Main"))
+    ]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return spicesLibrary.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -14,11 +21,12 @@ class LibraryVC: UIViewController, UITableViewDataSource {
         cell.libraryImageView.layer.cornerRadius = 9
         cell.layer.cornerRadius = 9
         cell.libraryImageView.image = #imageLiteral(resourceName: "DetailspiceCardHeader")
-        cell.spicesImageView.image = #imageLiteral(resourceName: "Ginger Main")
-        cell.spicesTitleLabel.text = "Ginger"
-        cell.spicesSubLabel.text = "Zingiber officinale"
+        cell.spicesImageView.image = spicesLibrary[indexPath.row].spiceImage
+        cell.spicesTitleLabel.text = spicesLibrary[indexPath.row].titleName
+        cell.spicesSubLabel.text = spicesLibrary[indexPath.row].subName
         cell.spicesView.layer.cornerRadius = 20
         cell.spicesView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        
         return cell
     }
     
